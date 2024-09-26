d6432897c06e0e572a87ab2fa107c576acd260b6
Yash Janbandhu <yashjanbandhu12@gmail.com>
2024-09-27 00:28:20 +0530

    medify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import SearchResults from './pages/SearchResults';
-// import BookingForm from './pages/BookingForm';
-import Banner from './components/Banner/Banner';
-import BookingsPage from './pages/BookingsPage';
-import HeroSection from './components/HeroSection';
+import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import Footer from "./components/Footer/Footer";
+import DownloadApp from "./components/Sections/DownloadApp/DownloadApp";
+import FAQs from "./components/Sections/FAQs/FAQs";
+
 function App() {
   return (
-    <Router>
-      <Banner />
-      <Navbar />
-      <HeroSection />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search-results" element={<SearchResults />} />
-        {/* <Route path="/book/:id" element={<BookingForm />} /> */}
-        <Route path="/my-bookings" element={<BookingsPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <CssBaseline />
+      <Outlet />
+      <FAQs />
+      <DownloadApp />
+      <Footer />
+    </div>
   );
 }
 
