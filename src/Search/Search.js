2c142085b9b4393a8d8cf62c42b64603a13ee110
Yash Janbandhu <yashjanbandhu12@gmail.com>
2024-09-27 22:14:10 +0530

    medify

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -25,7 +25,6 @@ export default function Search() {
   const [showBookingSuccess, setShowBookingSuccess] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  //API to fetch hospitals based on state and city selection
   useEffect(() => {
     const getHospitals = async () => {
       setHospitals([]);
