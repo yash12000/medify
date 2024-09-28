0827803eb62eeccfc9d3d5f2c171f2ffe8f174ad
Yash Janbandhu <yashjanbandhu12@gmail.com>
2024-09-28 17:42:28 +0530

    medify

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ import {
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
-import styles from "../NavBar/Navbar.module.css";
+import styles from "../NavBar/NavBar.module.css";
 import { useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import MenuIcon from "@mui/icons-material/Menu";
